2ba820d8f4760eeab6633e47f8e0ccd35e922d26
Jack Turek <jackturek@Jacks-MacBook-Pro.local>
2025-09-05 12:28:47 -0500

    Adding R file

---
+++ src/sentiments.R
@@ -0,0 +1,8 @@
+### Sentiment Analysis Between Texas & US Declarations of Independence
+## HS7345 US Borderlands
+# September 6, 2025
+
+# Load packages
+library(tidyverse)
+install.packages("syuzhet")
+library(syuzhet)
